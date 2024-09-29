cd7e09903dea43df90565aaf696d0ee1b615ac90
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-09-29 13:52:49 -0500

    imlement next auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
 import type { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
+
+import Link from "next/link";
+import { AuthButton } from "@/components/auth/sign-in";
+
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -7,6 +12,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -24,12 +30,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <SessionProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <header className="flex justify-between items-center p-4">
+            <h1 className="text-2xl font-bold">Next GPT Chat</h1>
+            <nav className="flex items-center gap-4">
+              <ul className="flex items-center gap-4">
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <Link href="/about">About</Link>
+                </li>
+                <li>
+                  <Link href="/dashboard">Dashboard</Link>
+                </li>
+              </ul>
+            </nav>
+            <AuthButton />
+          </header>
+          <main>{children}</main>
+          <footer>
+            <p>Next GPT Chat</p>
+          </footer>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
---
+++ src/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async signIn({ profile }) {
+      return profile?.login === "PaulBratslavsky";
+    },
+  },
+  providers: [GitHub],
+})
+
+
